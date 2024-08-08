c7b21ff3f1cea162c0fe823d03f1a78d2d48cf08
Saran Maihom <sarantoey8@gmail.com>
2024-08-08 15:33:34 +0700

    nt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 "use client";
+import Comment from "@/components/Comment";
+import { CommentProps } from "@/libs/types";
+import PostOwnner from "@/components/PostOwnner";
+import Reply from "@/components/Reply";
 
 export default function Home() {
   return (
@@ -11,56 +15,12 @@ export default function Home() {
         style={{ maxWidth: "700px", backgroundColor: "#242526" }}
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
-        {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
-            </span>
-          </div>
-
-          <span className="text-white">
-            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
-          </span>
-
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span style={{ color: "#B0B3B8" }}>100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
-
-        {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div>
+        {/* Post Owner */}
+        <PostOwnner username="Saran Maihom 660610796" userImagePath="/profileImages/My pic.jpg" 
+        statusText="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207" likeNum={100}/>
+    
+        {/* Comment */}
+        <Comment username="Lisa" userImagePath="/profileImages/lisa.jpg" commentText="จริงค่า" likeNum={999} />
 
         {/* Reply Example */}
         <div className="d-flex gap-2 my-2 ps-5">
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,5 +1,30 @@
 // You can define interfaces for all React components here.
 // Do not forget to export properly.
 
-interface ReplyProps {}
+interface ReplyProps {
+    userImagePath: string;
+    username: string;
+    replyText: string;
+    likeNum: number;
+}
 export type { ReplyProps };
+
+interface CommentProps { 
+    userImagePath: string;
+    username : string;
+    commentText : string;
+    likeNum : number;
+    replies : ReplyProps[];
+}
+export type { CommentProps };
+
+interface PostOwnnerProps {
+    userImagePath: string;
+    username: string;
+    statusText: string;
+    likeNum: number;
+}
+export type { PostOwnnerProps };
+
+
+
